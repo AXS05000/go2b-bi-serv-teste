2414f63351b18e40db1f63e1e783fa55b8a3a8b7
AXS05000 <axssxs00@gmail.com>
2023-06-29 22:40:02 -0300

    V - 29-06-2023 - 22:39

--- pdf/utils2.py
+++ pdf/utils2.py
@@ -5,6 +5,7 @@ from datetime import date, datetime, timedelta
 
 import openpyxl
 from dateutil.relativedelta import relativedelta
+from django.core.paginator import Paginator
 from django.http import FileResponse
 from reportlab.lib import colors, utils
 from reportlab.lib.pagesizes import landscape, letter
@@ -15,25 +16,32 @@ from .models import Beneficios_Mala, Folha_de_Ponto, Funcionario
 
 
 def importar_excel_beneficios(arquivo):
-    workbook = openpyxl.load_workbook(arquivo)
+    workbook = openpyxl.load_workbook(arquivo, read_only=True)
     sheet = workbook.active
 
-    for row in sheet.iter_rows(min_row=2, values_only=True):
-        Beneficios_Mala.objects.update_or_create(
-            id=row[0],
-            defaults={
-                'comp': row[1],
-                'codigo': row[2],
-                'codigo_fc': row[3],
-                'aut': row[4],
-                'data_inicio': row[5],
-                'data_fim': row[6],
-                'dias_calculados': row[7],
-                'tipo_de_beneficio': row[8],
-                'valor_pago': row[9],
-                'data_de_pagamento': row[10],
-            }
-        )
+    rows = list(sheet.iter_rows(min_row=2, values_only=True))  # Converter o gerador em uma lista
+
+    paginator = Paginator(rows, 1000)  # 1000 linhas por página
+
+    for page_number in paginator.page_range:
+        page = paginator.page(page_number)
+        for row in page.object_list:
+            Beneficios_Mala.objects.update_or_create(
+                id=row[0],
+                defaults={
+                    'comp': row[1],
+                    'codigo': row[2],
+                    'codigo_fc': row[3],
+                    'aut': row[4],
+                    'data_inicio': row[5],
+                    'data_fim': row[6],
+                    'dias_calculados': row[7],
+                    'tipo_de_beneficio': row[8],
+                    'valor_pago': row[9],
+                    'data_de_pagamento': row[10],
+                }
+            )
+
 
 
 
