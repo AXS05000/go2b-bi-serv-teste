80a6569debb3914a4a5866416b22f9985160ed9a
AXS05000 <axssxs00@gmail.com>
2023-07-01 22:47:40 -0300

    V - 01-07-2023 - 22:47

--- projeto_pdf/celery.py
+++ projeto_pdf/celery.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 
+import logging
 import os
 
 from celery import Celery
+from celery.signals import after_setup_logger
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_pdf.settings')
@@ -16,3 +18,10 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+@after_setup_logger.connect
+def setup_loggers(logger, *args, **kwargs):
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler = logging.FileHandler('celery.log')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
