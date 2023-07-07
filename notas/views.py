c49b5f42f104379059e2020791c60a42160846f5
AXS05000 <axssxs00@gmail.com>
2023-07-07 17:26:36 -0300

    V - 07-07-2023 - 16:20

--- notas/views.py
+++ notas/views.py
@@ -868,6 +868,12 @@ def generate_csv_for_nota(request, pk):
         'id_2': lambda nota: nota.id,
         'descricao': generate_description,
         '0': lambda nota: '0',
+        '0': lambda nota: '',
+        'base_pis': lambda nota: round(nota.total_a_faturar * Decimal("0.0065"), 4),
+        'base_confins': lambda nota: round(nota.total_a_faturar * Decimal("0.03"), 4),
+        'base_inss': lambda nota: round(nota.total_a_faturar * Decimal("0.11"), 4),
+        'base_ir': lambda nota: round(nota.total_a_faturar * Decimal("0.048"), 4),
+        'base_cssl': lambda nota: round(nota.total_a_faturar * Decimal("0.01"), 4),
     }
     sequential_number = 1  # iniciando o número sequencial para a segunda coluna
 
