03116891e112e28ad666b759ac19ad1af1cceac3
AXS05000 <axssxs00@gmail.com>
2023-09-20 13:15:15 -0300

    V - 20-09-2023 - 13:15

--- notas/views.py
+++ notas/views.py
@@ -609,7 +609,7 @@ def generate_csv(request):
             total_a_faturar_nota = round(nota.total_valor_outros, 2)
 
         else:
-            total_a_faturar_nota = format(nota.total_a_faturar_nota, '.4f')
+            total_a_faturar_nota = format(nota.total_a_faturar, '.4f')
 
         base_pis = total_a_faturar_nota * Decimal("0.0065")
         base_confins = total_a_faturar_nota * Decimal("0.03")
